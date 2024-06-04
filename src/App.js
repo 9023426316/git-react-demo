ee8ef0680a3ec4a14c379c2be29c7a8a5abd39bd
9023426316 <princeakbari0077@gmail.com>
2024-06-04 16:28:27 +0530

    add file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import img1 from './images/img-1.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Our Gallery</li>
+          <li>Student Corner</li>
+        </ul>
+      </nav>
+
+      <p className="lorem-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum eos et minus magnam dolorum delectus itaque assumenda deleniti quis! Quis at voluptas soluta ducimus, ipsam quae fuga. Atque, beatae necessitatibus.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum eos et minus magnam dolorum delectus itaque assumenda deleniti quis! Quis at voluptas soluta ducimus, ipsam quae fuga. Atque, beatae necessitatibus.</p>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrKHPsvNDJHY9tWpkHrfkfo8Dkf0LvZU3Hdg&s" className="image-1" alt=""  />
+      <br></br>
+      <img src={img1} alt="" className="image-2" />
+    </header>
+      </>
   );
 }
 
