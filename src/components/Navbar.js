f3929c92869e2ef65286964f46ea1f37e4790781
9023426316 <princeakbari0077@gmail.com>
2024-06-21 17:37:53 +0530

    add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-
-
 export default function Navbar(props) {
     return (
         <div>
             <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
-               
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,34 +18,28 @@ export default function Navbar(props) {
                             <li className="nav-item">
                                 <a className="nav-link" href="#">Link</a>
                             </li>
-
                             <li className="nav-item">
                                 <a className="nav-link disabled" aria-disabled="true">{props.aboutText}</a>
                             </li>
                         </ul>
                         <form className="d-flex" role="search">
-                        <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark': 'light'}`}>
-                          <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode}/>{props.btntext} 
-
-                          
-
-
-                          
-                        </div>
+                            <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode} />{props.btntext}
+                            </div>
                         </form>
                     </div>
                 </div>
             </nav>
         </div>
-
-        
     )
 }
-Navbar.propTypes={
-    title : PropTypes.string.isRequired,
-    aboutText : PropTypes.string
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    aboutText: PropTypes.string
 }
-Navbar.defaultProps={
-    title : "Set Your Title Here",
-    aboutText :"Set Your Text Here", 
+
+Navbar.defaultProps = {
+    title: "Set Your Title Here",
+    title: "set your text here"
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,74 +1,75 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 export default function TextForm(props) {
-    var [text, setText]= useState("input your text");
+  var [text, setText] = useState("input your text");
 
-    const handleonchange = (event) => {
-      setText(event.target.value)
-    }
+  const handleonchange = (event) => {
+    setText(event.target.value)
+  }
 
-    const handUpchange = () => {
-      let newText = text.toUpperCase();
-      setText(newText);
-    }
+  const handUpchange = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
 
-    const handleUpchange = () => {
-      let newText = text.toLowerCase();
-      setText(newText);
-    }
+  const handUpchangee = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
 
-    
-    const handlespeak = () => {
-      let newText = new SpeechSynthesisUtterance();
-      newText.text = text;
-      window.speechSynthesis.speak(newText);
-    }
 
-    const handleclear = () => {
-      let newText = "";
-      setText(newText);
-      
-    }
 
-    const handleCopy = () => {
-      var newText = document.getElementById('exampleFormControlTextarea1');
-      newText.select();
-      navigator.clipboard.writeText(newText.value);
-    }
-    
-    const handleRemoveSpace = () => {
-      let newText = text.split(/ []+ /);
-      setText(newText.join(""));
-    }
+  const handlespeak = () => {
+    let newText = new SpeechSynthesisUtterance();
+    newText.text = text;
+    window.speechSynthesis.speak(newText);
+  }
+
+  const handleclear = () => {
+    let newText = "";
+    setText(newText);
+
+  }
+
+  const handlecopy = () => {
+    var newText = document.getElementById('exampleFormControlTextarea1');
+    newText.select();
+    navigator.clipboard.writeText(newText.value);
+  }
+
+  const handleRemoveSpace = () => {
+    let newText = text.split(/[ ] + /);
+    setText(newText.join(" "));
+  }
 
   return (
     <div>
       <div className="mb-3 mx-5 my-5">
         <h1>{props.heading}</h1>
-  <textarea className={`form-control bg-${props.mode === 'light' ? 'light': 'dark'} text-${props.mode === 'light' ? 'dark': 'light'}`} id="exampleFormControlTextarea1" rows="3" value={text} onChange={handleonchange}></textarea>
-  <button className='btn btn-outline-success my-3' onClick={handUpchange}>Convert to upper btn</button>
+        <textarea className={`form-control bg-${props.mode === 'light' ? 'light' : 'dark'} text-${props.mode === 'light' ? 'dark' : 'light'}`} id="exampleFormControlTextarea1" rows="3" value={text} onChange={handleonchange}></textarea>
+        <button className='btn btn-outline-success my-3' onClick={handUpchange}>Convert to upper btn</button>
+        &nbsp;&nbsp;
+        <button className='btn btn-outline-success my-3' onClick={handUpchangee}>Convert to lower btn</button>
+        &nbsp;&nbsp;
+        <button className='btn btn-outline-success my-3' onClick={handlespeak}>speak</button>
+        &nbsp;&nbsp;
+        <button className='btn btn-outline-success my-3' onClick={handleclear}>Clear</button>
+        &nbsp;&nbsp;
+        <button className='btn btn-outline-success my-3' onClick={handlecopy}>Copy</button>
+        &nbsp;&nbsp;
+        <button className='btn btn-outline-success my-3' onClick={handleRemoveSpace}>Remove Space</button>
 
-  <button className='btn btn-outline-secondary my-3 mx-3' onClick={handleUpchange}>Convert to lower btn</button>
+      </div>
 
-  <button className='btn btn-outline-success my-3 mx-3' onClick={handlespeak}>speak</button>
+      <div className='container'>
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length}Words</p>
+        <p>{text.length}Characters</p>
+        <p>{0.008 * text.split(" ").length}reading time</p>
+        <h3>preview</h3>
+        <p>{text}</p>
+      </div>
+    </div>
 
-  <button className='btn btn-outline-success my-3 mx-3' onClick={handleclear}>Clear</button>
-
-  <button className='btn btn-outline-success my-3 mx-3' onClick={handleCopy}>Copy</button>
-
-  <button className='btn btn-outline-success my-3 ' onClick={handleRemoveSpace}>Remove Space</button>
-
-
-  </div>
-
-  <div className='container'>
-    <h2>Your text summary</h2>
-    <p>{text.split(" ").length}Words</p>
-    <p>{text.length}Characters</p>
-    <p>{0.008 * text.split(" ").length}reading time</p>
-    <h3>preview</h3>
-    <p>{text}</p>
-  </div>
-</div>
-    
-  )}
+  )
+}
